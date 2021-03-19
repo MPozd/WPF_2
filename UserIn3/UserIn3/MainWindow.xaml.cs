818a6e3ca37f35e061b87bfef8e3ac5fa4187d8b
MPozd <47981972+MPozd@users.noreply.github.com>
2021-03-19 23:43:21 +0300

    Закончена Лаб2

--- UserIn3/UserIn3/MainWindow.xaml.cs
+++ UserIn3/UserIn3/MainWindow.xaml.cs
@@ -23,11 +23,36 @@ namespace UserIn3
         public MainWindow()
         {
             InitializeComponent();
+            foreach (FontFamily F in Fonts.SystemFontFamilies)
+            {
+                comboBox1.Items.Add(F.ToString());
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            richTextBox1.Selection.ApplyPropertyValue(FontWeightProperty, FontWeights.Bold);
         }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            richTextBox1.Selection.ApplyPropertyValue(FontStyleProperty, FontStyles.Italic);
+        }
+
+        private void listBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            richTextBox1.Selection.ApplyPropertyValue(FontFamilyProperty, ((ListBoxItem)listBox1.SelectedItem).FontFamily);
+        }
+
+        /*
+        private void Slider1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            try
+            {
+                richTextBox1.Selection.ApplyPropertyValue(FontSizeProperty, Slider1.Value.ToString());
+            }
+            catch { }
+
+        }*/
     }
 }
